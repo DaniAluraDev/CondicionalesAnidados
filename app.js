02019969a7701ea3893c3cd0f9507f9966b4b323
Daniel Sanchez <programazion1@outlook.com>
2024-06-29 21:56:14 -0500

    Aplicación del while

--- app.js
+++ app.js
@@ -1,38 +1,27 @@
-/*//variables
-let numeroSecreto = 8;
-let numeroUsuario= prompt("Me indicas un numero del 1 al 10 por favor: ");
-
-console.log(numeroUsuario);
-/*Este código realiza la comparación
- */ 
-/*if (numeroUsuario==numeroSecreto){
-    //Acertamos fue verdadera la condición
-    alert(`acertaste el numero es: ${numeroUsuario}`);
-}else {
-    //No acertamos, no fue verdadera la condición
-    alert('Lo siento, no acertaste el numero');
-}
-**/
 alert('Bienvenido al juego del número secreto');
 
 // Agrega un console.log para verificar el valor de "intento" después de la entrada del usuario
-let intento = prompt('Elige un número entre 1 y 10');
-console.log('Valor de intento:', intento);
 
 let numeroSecreto = 4;
+let intento;
+let i =1;
+palabra = 'intento';
+while (intento != numeroSecreto){
+    intento = prompt('Elige un número entre 1 y 10');    
 
-// Agrega un console.log para verificar la comparación entre "intento" y "numeroSecreto"
-console.log('Resultado de la comparación:', intento == numeroSecreto);
-
-if (intento == numeroSecreto) {
-    alert('Adivinaste');
-} else {
-    if (intento < numeroSecreto){
-        alert ('El numero es menor');
+    if (intento == numeroSecreto) {
+        alert(`Adivinaste, el numero es: ${intento}. lo hiciste en ${i} ${palabra}`);
     } else {
-        alert ('El numero es mayor');
-    }
-    // Agrega un console.log para verificar el valor de "numeroSecreto" cuando el jugador se equivoca
-    //console.log('Valor del número secreto:', numeroSecreto);
-    //alert('El número secreto era ' + numeroSecreto);
-}
+        if (intento < numeroSecreto){
+            alert ('El numero es menor');
+        } else {
+            alert ('El numero es mayor');
+        }
+        //Contador de intentos incrementable
+        i = i +1;
+        palabra = 'intentos';
+        // Agrega un console.log para verificar el valor de "numeroSecreto" cuando el jugador se equivoca
+        //console.log('Valor del número secreto:', numeroSecreto);
+        //alert('El número secreto era ' + numeroSecreto);
+    } 
+} 
